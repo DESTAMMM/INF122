c667c1214bbbc12912e4a0864141fd060f1fd027
Destiny <155449292+DESTAMMM@users.noreply.github.com>
2024-01-15 15:54:41 +0000

    V. semana 3 dia 1

--- semana3/segundo-proyecto/src/App.js
+++ semana3/segundo-proyecto/src/App.js
@@ -1,22 +1,46 @@
 import './App.css';
 import Contador from './components/Contador';
 import Boton from './components/Boton';
+import { useState } from 'react';
 function App() {
+  const[nroClicks, setNumClicks]=useState(0);
+  const[Show,setShow]=useState(true);
+  const reducir3=()=>{
+    setNumClicks(nroClicks-3)
+  }
+  const aumentar3=()=>{
+    setNumClicks(nroClicks+3)
+  }
+  const reducir1=()=>{
+    setNumClicks(nroClicks-1)
+  }
   const click=()=>{
+    setNumClicks(nroClicks+1);
     console.log("1");
   }
   const reiniciar=()=>{
+    setNumClicks(0);
     console.log("reiniciar");
+  }
+  const mostrar=()=>{
   }
   return (
     <div className="App">
       <div className="contenedor-principal">
-        <Contador nroClicks={0}/>
+        <Contador nroClicks={nroClicks} />
         <div className='contenedor-botones'>
-          <Boton texto="Click" esBotonClick={true}
-            funcionClick={click}/>
-          <Boton texto="Reiniciar" esBotonClick={false}
-            funcionClick={reiniciar}/>
+          <div className='botones'>
+            <Boton texto="-3" esBotonClick={true}
+              funcionClick={reducir3} />
+            <Boton texto="+3" esBotonClick={true}
+              funcionClick={aumentar3} />
+            <Boton texto="-1" esBotonClick={true}
+              funcionClick={reducir1} />
+            <Boton texto="+1" esBotonClick={true}
+              funcionClick={click} />
+          </div>
+          <Boton className="reset" texto="Reiniciar" esBotonClick={false}
+            funcionClick={reiniciar} />
         </div>
       </div>
     </div>
