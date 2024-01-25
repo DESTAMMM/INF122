8b05cab838ca41ff1548ac5c3c80152598de8438
Destiny <155449292+DESTAMMM@users.noreply.github.com>
2024-01-25 15:40:38 +0000

    V. s4 d4 apis

--- semana4/dogs-galery/next.config.mjs
+++ semana4/dogs-galery/next.config.mjs
@@ -3,7 +3,8 @@ const nextConfig = {
     images:{
         remotePatterns:[
             {
-                hostname:'images.dog.ceo',
+                //hostname:'images.dog.ceo',
+                hostname:'raw.githubusercontent.com',
             }
         ],
     }
--- semana4/dogs-galery/src/app/components/Foto.js
+++ semana4/dogs-galery/src/app/components/Foto.js
@@ -4,13 +4,17 @@ import { useState, useEffect } from "react";
 import style from './Foto.module.css';
 function Foto (){
     const [perrito,setPerrito]=useState("/vercel.svg");
-    const url="https://dog.ceo/api/breeds/image/random";
+    const [estado, setEstado]=useState("esperando");
+    const [nom,setNom]=useState(" ");
+    const url="https://pokeapi.co/api/v2/pokemon/squirtle";
     useEffect(()=>{
         fetch(url).then(respuesta=>respuesta.json())
-        .then(datos=>{setPerrito(datos["message"])})
+        .then(datos=>{setPerrito(datos.sprites.front_default),setEstado(datos.abilities[0].ability.name, setNom(datos.species.name))})
     },[]);
     return(
         <div className={style.contenedor}>
+            <h1>{estado}</h1>
+            <h1>{nom}</h1>
             <Image src={perrito} alt="perrito" width={500} height={500}></Image>
         </div>
     );
